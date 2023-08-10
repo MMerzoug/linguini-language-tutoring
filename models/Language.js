b311fde1b677bbf1be59bc2ddf5f5bd6ae5064d1
MMerzoug <monique.merzoug@icloud.com>
2023-08-09 22:08:09 -0700

    added code to language.js and tutor.js

--- models/Language.js
+++ models/Language.js
@@ -1,3 +1,27 @@
-// Languages
-// Id
-// Name
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Language extends Model {}
+
+Language.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+  },
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'painting', 
+}
+);
+
+module.exports = Language;
