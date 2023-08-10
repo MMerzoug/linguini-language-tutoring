22bafdfb9a5b10aa3c8c2afb5e5082a505783bfe
MMerzoug <monique.merzoug@icloud.com>
2023-08-10 00:06:12 -0700

    updated tutorPivotLanguage model

--- models/tutorLanguageLevel.js
+++ models/tutorLanguageLevel.js
@@ -3,3 +3,44 @@
 // id
 // tutor_id (FK to Tutors.id)
 // Language_id (FK to Languages.id)
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class TutorLanguagePivot extends Model { }
+
+TutorLanguagePivot.init(
+    {
+        id: {
+            typeof: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        tutor_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'tutor',
+                key: 'id',
+            },
+        },
+        language_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'language',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tutor_language_pivot',
+    }
+);
+
+module.exports = TutorLanguagePivot;
