4afa500dfd0b9a0d9d96acf29523a8834f9303e0
Jesus Santos <jo_nez@hotmail.com>
2023-08-10 22:55:57 -0700

    userSeeds are working!

--- models/index.js
+++ models/index.js
@@ -1,43 +1,39 @@
 const User = require ('./User');
-const Tutor = require ('./Tutor');
-const Language = require ('./Language');
-const TutorRating = require ('./tutorRating');
+// const Tutor = require ('./Tutor');
+// const Language = require ('./Language');
+// const TutorRating = require ('./TutorRating');
+// const Student = require('./Student');
 
 // A user can have many languages
-User.hasOne(Language, {
-  foreignKey: 'user_id',
-});
+// User.hasOne(Language, {
+//   foreignKey: 'user_id',
+// });
 
-Language.belongsTo(User,{
-    foreignKey: 'user_id',
-});
+// Language.belongsTo(User,{
+//     foreignKey: 'user_id',
+// });
 
-Tutor.hasOne(Language, {
-    foreignKey: 'tutor_id',
-});
+// Tutor.hasOne(Language, {
+//     foreignKey: 'tutor_id',
+// });
 
-Language.belongsTo(Tutor,{
-    foreignKey: 'tutor_id',
-});
+// Language.belongsTo(Tutor,{
+//     foreignKey: 'tutor_id',
+// });
 
-Tutor.hasMany(User, {
-    foreignKey: 'tutor_id'
-});
+// Tutor.hasMany(User, {
+//     foreignKey: 'tutor_id'
+// });
 
-User.belongsTo(Tutor,{
-    foreignKey: 'tutor_id',  
-});
-
+// User.belongsTo(Tutor,{
+//     foreignKey: 'tutor_id',  
+// });
 
 // Export the models for use in other parts of the app
 module.exports = {
     User,
-    Tutor,
-    Language,
-    TutorRating,
+    // Tutor,
+    // Language,
+    // TutorRating,
+    // Student
   };
-  
-
-
-
-
--- seeds/index.js
+++ seeds/index.js
@@ -1,25 +1,25 @@
 const sequelize = require('../config/connection.js');
 const {
   User,
-  Language,
-  Message,
-  ScheduledSession,
-  Student,
-  StudentLanguageLevel,
-  StudentTutorPivot,
-  Tutor,
-  TutorRating
+  // Language,
+  // Message,
+  // ScheduledSession,
+  // Student,
+  // StudentLanguageLevel,
+  // StudentTutorPivot,
+  // Tutor,
+  // TutorRating
 } = require('../models');
 
 const userData = require('./userSeeds.json');
-const languageData = require('./languageSeeds.json');
-const messageData = require('./messageSeeds.json');
-const scheduledSessionData = require('./scheduledSessionSeeds.json');
-const studentData = require('./studentSeeds.json');
-const studentLanguageLevelData = require('./studentLanguageLevelSeeds.json');
-const studentTutorPivotData = require('./studentTutorPivotSeeds.json');
-const tutorData = require('./tutorSeeds.json');
-const tutorRatingData = require('./tutorRatingSeeds.json');
+// const languageData = require('./languageSeeds.json');
+// const messageData = require('./messageSeeds.json');
+// const scheduledSessionData = require('./scheduledSessionSeeds.json');
+// const studentData = require('./studentSeeds.json');
+// const studentLanguageLevelData = require('./studentLanguageLevelSeeds.json');
+// const studentTutorPivotData = require('./studentTutorPivotSeeds.json');
+// const tutorData = require('./tutorSeeds.json');
+// const tutorRatingData = require('./tutorRatingSeeds.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -29,45 +29,45 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  await Language.bulkCreate(languageData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await Language.bulkCreate(languageData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await Message.bulkCreate(messageData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await Message.bulkCreate(messageData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await ScheduledSession.bulkCreate(scheduledSessionData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await ScheduledSession.bulkCreate(scheduledSessionData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await Student.bulkCreate(studentData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await Student.bulkCreate(studentData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await StudentLanguageLevel.bulkCreate(studentLanguageLevelData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await StudentLanguageLevel.bulkCreate(studentLanguageLevelData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await StudentTutorPivot.bulkCreate(studentTutorPivotData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await StudentTutorPivot.bulkCreate(studentTutorPivotData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await Tutor.bulkCreate(tutorData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await Tutor.bulkCreate(tutorData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
-  await TutorRating.bulkCreate(tutorRatingData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await TutorRating.bulkCreate(tutorRatingData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
   process.exit(0);
 };
