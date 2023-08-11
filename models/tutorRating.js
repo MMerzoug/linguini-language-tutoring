82b59fdea01224f76fa8067a08e2e3808d5764e4
athenamw <wallisathena@gmail.com>
2023-08-11 12:42:29 -0700

    Verified server running

--- models/tutorRating.js
+++ models/tutorRating.js
@@ -5,7 +5,6 @@
 // Review (text filed type)
 // Submitted_by
 
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const Tutor = require('./Tutor');
@@ -13,52 +12,52 @@ const Tutor = require('./Tutor');
 class TutorRating extends Model {}
 
 TutorRating.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        tutor_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'tutor',
-                key: 'id',
-            },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        },
-        rating: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            validate: {
-                isNumeric: true,
-                len: [1, 5],
-            },
-        },
-        review: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        student_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'student',
-                key: 'id',
-            },
-            onUpdate: 'CASCADE',
-            onDelete: 'CASCADE',
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'tutorRating',    
-    }
+    tutor_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tutor',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        isNumeric: true,
+        len: [1, 5],
+      },
+    },
+    review: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    student_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'student',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tutorRating',
+  }
 );
 
 module.exports = TutorRating;
