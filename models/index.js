704d36b729261c105cc964be9d98644ace017287
athenamw <wallisathena@gmail.com>
2023-08-11 22:14:49 -0700

    fixed seeds

--- models/index.js
+++ models/index.js
@@ -1,24 +1,28 @@
 const User = require('./User');
 const Tutor = require('./Tutor');
-// const Language = require ('./Language');
+const Language = require('./Language');
 // const TutorRating = require ('./TutorRating');
 const Student = require('./Student');
-const ScheduledSession = require('./ScheduledSession');
+// const ScheduledSession = require('./ScheduledSession');
 
 Student.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-Tutor.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+// Tutor.belongsTo(User, {
+//   foreignKey: 'user_id',
+// });
 // A user can have many languages
 // User.hasOne(Language, {
 //   foreignKey: 'user_id',
 // });
 
-// Language.belongsTo(User,{
-//     foreignKey: 'user_id',
+Language.belongsTo(Student, {
+  foreignKey: 'user_id',
+});
+
+// Language.belongsTo(Tutor, {
+//   foreignKey: 'user_id',
 // });
 
 // Tutor.hasOne(Language, {
@@ -37,16 +41,16 @@ Tutor.belongsTo(User, {
 //     foreignKey: 'tutor_id',
 // });
 
-ScheduledSession.belongsTo(Tutor, {
-  foreignKey: 'tutor_id',
-});
+// ScheduledSession.belongsTo(Tutor, {
+//   foreignKey: 'tutor_id',
+// });
 
 // Export the models for use in other parts of the app
 module.exports = {
   User,
   Tutor,
-  // Language,
+  Language,
   // TutorRating,
   Student,
-  ScheduledSession,
+  // ScheduledSession,
 };
