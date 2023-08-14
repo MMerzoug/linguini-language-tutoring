78a3dc965eb85798f4b2d04e41aea9f24c06362f
MMerzoug <monique.merzoug@icloud.com>
2023-08-13 22:18:04 -0700

    updated message post route

--- controllers/api/messageController.js
+++ controllers/api/messageController.js
@@ -20,13 +20,30 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.post('/', async (req, res) => {
-  // const messageData = await Message.create(req.body);
-  //use message_id to create notification
-// include message and notification in post
-// create the message and notification in the same post
-// });
+router.post('/', async (req, res) => {
+  let transaction;
+  try {
+    // Start a new transaction
+    transaction = await Message.sequelize.transaction();
 
+    // Create the message
+    const messageData = await Message.create(req.body, { transaction });
 
+    // Use message ID to create a notification
+    const notificationData = await Notification.create({
+      message_id: messageData.id,
+      // any other necessary fields for Notification
+    }, { transaction });
+
+    // Commit the transaction
+    await transaction.commit();
+
+    res.status(201).json({ messageData, notificationData });
+  } catch (err) {
+    // If there's any error, rollback the transaction
+    if (transaction) await transaction.rollback();
+    res.status(400).json(err);
+  }
+});
 
 module.exports = router;
