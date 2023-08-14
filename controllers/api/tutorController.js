31ee9a7ae95de2ad016dd2bbc934f2f8024c38e4
MMerzoug <monique.merzoug@icloud.com>
2023-08-14 00:25:01 -0700

    fixed user route

--- controllers/api/tutorController.js
+++ controllers/api/tutorController.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Tutor, Language } = require('../../models');
+const { User, Tutor, Language, TutorRating } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
@@ -43,9 +43,35 @@ router.get("/:language", async (req, res) => {
   }
 });
 
-
-
 //post route separate from the user
-
+router.post('/create', async (req, res) => {
+  try {
+    const userData = await User.create({
+      // ...req.body,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const LanguagelevelData = await LanguageLevel.findOne({
+      where: {
+        label: req.body.language_level,
+      }
+    });
+    const languageData = await Language.findOne({
+      where: {
+        name: req.body.language,
+      }
+    });
+    const tutorData = await Tutor.create({
+      user_id: userData.id,
+      rating: ratingData.id,
+      language_id: languageData.id,
+    });
+    res.status(200).json(studentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 const port = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
