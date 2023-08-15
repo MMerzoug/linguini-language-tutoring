c78d3822441a77ff6fe4fae788c1c18267b17e07
athenamw <wallisathena@gmail.com>
2023-08-14 21:31:33 -0700

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
   });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'process.env.DB_HOST',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306,
     logging: console.log,
