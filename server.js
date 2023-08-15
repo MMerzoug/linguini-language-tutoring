41f1d2550151f86fe10142dcf09f4e7b561ec321
athenamw <wallisathena@gmail.com>
2023-08-14 19:03:53 -0700

    fixed spelling on server.js

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
-tutorRatingCOntroller
+tutorRatingController;
 const exphbs = require('express-handlebars');
 const path = require('path');
 
 const session = require('express-session');
-
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -30,12 +29,11 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -43,7 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // Start the server
-sequelize.sync({force:false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
