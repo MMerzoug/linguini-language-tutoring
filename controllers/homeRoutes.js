ca2ba62c2e90195ca987ad605d8035f764490a40
MMerzoug <monique.merzoug@icloud.com>
2023-08-15 15:36:16 -0700

    added handlebars for homepage, student profile page and student listing page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,44 @@
 const router = require('express').Router();
-const { User, Tutor} = require('../models');
+const { User, Tutor } = require('../models');
 
-//localhost:3001/
+// Render tutors on the homepage
 router.get('/', async (req, res) => {
-    const tutorData = await Tutor.findAll({
-        include:[User]
-    })
-    //console.log(tutorData)
-    const tutors= tutorData.map((tutor) => tutor.get({ plain: true }));
-    console.log(tutors)
-    res.render("homepage.handlebars", {tutors})
-})
+    try {
+        const tutorData = await Tutor.findAll({ 
+            include: [User] 
+        });
+        const tutors = tutorData.map(tutor => tutor.get({ plain: true }));
+        res.render("homepage", { tutors }); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("An error occurred");
+    }
+});
+
+// Render user data on the tutorListing page 
+router.get('/tutorListing', async (req, res) => {
+    try {
+        const tutorData = await Tutor.findAll({
+            include: [User]
+        });
+        const tutors = tutorData.map(tutor => tutor.get({ plain: true }));
+        res.render("tutorListing", { tutors});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("An error occurred");
+    }
+});
 
 router.get('/tutorProfile', async (req, res) => {
-    res.render("tutorProfile.handlebars")
-})
+    res.render("tutorProfile.handlebars");  
+});
 
 router.get('/studentProfile', async (req, res) => {
-    res.render("studentProfile.handlebars")
-})
+    res.render("studentProfile.handlebars");  
+});
+
+router.get('/tutorListing', async (req, res) => {
+    res.render("tutorListing.handlebars");  
+});
 
 module.exports = router;
