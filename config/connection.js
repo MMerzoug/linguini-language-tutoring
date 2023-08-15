27c532076d683a6228c36f3d03d9ddbccb6e317c
athenamw <wallisathena@gmail.com>
2023-08-14 19:26:31 -0700

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,14 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   // sequelize = new Sequelize(process.env.JAWSDB_URL);
   sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    logging: console.log
+    logging: console.log,
   });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: "127.0.0.1",
+    host: 'process.env.DB_HOST',
     dialect: 'mysql',
     port: 3306,
-    logging: console.log 
+    logging: console.log,
   });
 }
 
