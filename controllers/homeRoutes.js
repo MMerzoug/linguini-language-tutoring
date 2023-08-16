956a9b205fd7aa92467c0426e23af18ea1ad6ee5
MMerzoug <monique.merzoug@icloud.com>
2023-08-15 21:22:44 -0700

    updated handlebars removing res.render lines and added tutorProfile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Tutor } = require('../models');
+const { User, Tutor, Student } = require('../models');
 
 // Render tutors on the homepage
 router.get('/', async (req, res) => {
@@ -31,20 +31,45 @@ router.get('/tutorListing', async (req, res) => {
 });
 
 // Render student profile on the studentListing page 
+router.get('/studentProfile/:id', async (req, res) => {
+    try {
+      const studentData = await Student.findOne({
+        include: [
+            {
+            model: User,
+          },
+        ],
+        where: {
+          id: req.params.id,
+        },
+      });
+      const student = studentData.get({ plain: true });
+      res.render("studentProfile", {student});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("An error occurred");
+    }
+  });
 
 // Render tutor profile on the tutorProfile page 
-
-
-router.get('/tutorProfile', async (req, res) => {
-    res.render("tutorProfile.handlebars");  
-});
-
-router.get('/studentProfile', async (req, res) => {
-    res.render("studentProfile.handlebars");  
-});
-
-router.get('/tutorListing', async (req, res) => {
-    res.render("tutorListing.handlebars");  
-});
+router.get('/tutorProfile/:id', async (req, res) => {
+    try {
+      const tutorData = await Tutor.findOne({
+        include: [
+            {
+            model: User,
+          },
+        ],
+        where: {
+          id: req.params.id,
+        },
+      });
+      const tutor = tutorData.get({ plain: true });
+      res.render("tutorProfile", {tutor});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("An error occurred");
+    }
+  });
 
 module.exports = router;
