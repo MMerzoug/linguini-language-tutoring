d0534cb8fd1ca45bc566b132b7f3352a4dc234ec
MMerzoug <monique.merzoug@icloud.com>
2023-08-17 11:11:13 -0700

    updated scheduled session. It works, however it needs to be updated to reference names not id's

--- public/js/scheduledSession.js
+++ public/js/scheduledSession.js
@@ -1,37 +1,62 @@
 // Handle the form submission event
-const form = document.getElementById('schedule-tutoring-session');
+// const form = document.getElementById('schedule-tutoring-session');
 
-form.addEventListener('submit', async (event) => {
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+// Get the student ID, tutor ID, session time, and topic from the form
+// const studentId = document.getElementById('student-id').value;
+// const tutorId = document.getElementById('tutor-id').value;
+// const sessionTime = document.getElementById('session-time').value;
+// const topic = document.getElementById('topic').value;
+
+// Create a new Tutor object
+// const tutor = new Tutor({
+//   name: 'John Doe',
+//   email: 'johndoe@example.com',
+//   phone: '123-456-7890',
+//   availability: ['Monday 10am-12pm', 'Wednesday 2pm-4pm'],
+// });
+
+// Save the Tutor object
+// await tutor.save();
+
+// Create a new ScheduledSession object
+// const scheduledSession = new ScheduledSession({
+//   studentId,
+//   tutorId,
+//   sessionTime,
+//   topic,
+// });
+
+// Save the ScheduledSession object
+// await scheduledSession.save();
+
+// Display a success message
+//   alert('Tutoring session scheduled successfully! Your tutor will provide an email with a link to the session.');
+// });
+
+const postSession = async (event) => {
   event.preventDefault();
 
-  // Get the student ID, tutor ID, session time, and topic from the form
   const studentId = document.getElementById('student-id').value;
   const tutorId = document.getElementById('tutor-id').value;
-  const sessionTime = document.getElementById('session-time').value;
-  const topic = document.getElementById('topic').value;
+  const sessionBegin = document.getElementById('session-begin').value;
+  const sessionEnd = document.getElementById('session-end').value;
+  const meetingLink = document.getElementById('meeting-link').value;
 
-  // Create a new Tutor object
-  const tutor = new Tutor({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    phone: '123-456-7890',
-    availability: ['Monday 10am-12pm', 'Wednesday 2pm-4pm'],
+// api fetch should align to the index.js in api folder
+  const response = await fetch('/api/scheduledSessions', {
+    method: 'POST',
+    body: JSON.stringify({ student_id:studentId, tutor_id:tutorId, session_begin: sessionBegin,  session_end:sessionEnd , meeting_link:meetingLink }),
+    headers: { 'Content-Type': 'application/json' },
   });
+  if (response.ok) {
+    alert ("Tutoring session scheduled successfully! Your tutor will provide an email with a link to the session");
+    document.location.replace('/scheuledSession');
+  } else {
+    alert(response.statusText);
+  }
+};
 
-  // Save the Tutor object
-  await tutor.save();
-
-  // Create a new ScheduledSession object
-  const scheduledSession = new ScheduledSession({
-    studentId,
-    tutorId,
-    sessionTime,
-    topic,
-  });
-
-  // Save the ScheduledSession object
-  await scheduledSession.save();
-
-  // Display a success message
-  alert('Tutoring session scheduled successfully! Your tutor will provide an email with a link to the session.');
-});
+document.getElementById("scheduled-session-form").addEventListener("submit", postSession)
