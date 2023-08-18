95536e0fa73b006d6799a27360c5d79f722c67af
athenamw <wallisathena@gmail.com>
2023-08-17 18:51:19 -0700

    added closing html

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,11 @@
 let signUpBtn = document.getElementById('signUpBtn');
 let clearBtn = document.getElementById('clearBtn');
 
+// signUpBtn.addEventListener('click', async (event) => {
+//   event.preventDefault();
+//   const response = await fetch('/api/');
+// });
+
 clearBtn.addEventListener('click', async (event) => {
   event.preventDefault();
   document.getElementById('signUpForm').reset();
