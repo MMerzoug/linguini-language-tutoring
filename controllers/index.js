39647aea8f68d390d355ca23a23e317b0f248503
MMerzoug <monique.merzoug@icloud.com>
2023-08-22 00:21:46 -0700

    Added date slot to handlebars. Still not displaying the correct date

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const apiRoutes = require('./api');
+const dayjs = require('dayjs');
+dayjs().format();
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
